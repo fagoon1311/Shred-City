dc714d6872c4a600b382c59dc47eef77f2245d60
fagoon1311 <fs14smailbox65@gmail.com>
2024-09-10 22:05:46 +0530

    ..

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,11 +1,14 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const LandingPage = () => {
   return (
     <div>
       <main>
-        <section className="relative section-bg min-h-screen flex items-center justify-center">
-          <h1 className="font-Poppins text-white z-10  lg:text-8xl font-semibold md:text-6xl">Get Fit</h1>
+        <section className="relative section-bg min-h-screen flex flex-col items-center justify-center px-6">
+          <h1 className="font-Poppins text-white z-10  lg:text-8xl font-semibold md:text-6xl">Crush your health and <br></br>fitness goals in no time.</h1>
+          <Button variant='neon' className='mt-10 hover:cur' size='xl'>Start your Trial</Button>
+
         </section>
       </main>
 
